6a0f077669c429f24cf1e98d0d67a2c142637659
arif <arifmr91@gmail.com>
2017-01-04 11:35:00 +0530

    first commit

---
+++ controller.js
@@ -0,0 +1,75 @@
+
+var app = angular.module('myApp', []);
+
+app.service('Mathservice',function () {
+     this.multiply = function (a,b) {
+         return a * b
+     }
+
+    this.devide = function (a,b) {
+        return a / b
+    }
+
+})
+
+app.service('Calculateservice',function (Mathservice) {
+    this.square = function (a) {
+        return Mathservice.multiply(a,a)
+
+    }
+    this.cube=function (a) {
+        return Mathservice.multiply(a,Mathservice.multiply(a,a))
+
+    }
+
+})
+
+app.controller('myCtrl', function($scope,Calculateservice) {
+    $scope.dosquare = function () {
+        console.log('square calling')
+        $scope.answer = Calculateservice.square($scope.number);
+    }
+
+    $scope.docube = function () {
+        console.log('cube calling')
+        $scope.answer = Calculateservice.cube($scope.number)
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
